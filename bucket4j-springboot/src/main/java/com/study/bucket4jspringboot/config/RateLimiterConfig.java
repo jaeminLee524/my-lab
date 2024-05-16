098d89c67629a758b42bb76b2df44d58f1d1df1b
이재민 <rkwoals524@hanmail.net>
2024-05-16 22:23:20 +0900

    StatefulRedisConnection 사용

--- bucket4j-springboot/src/main/java/com/study/bucket4jspringboot/config/RateLimiterConfig.java
+++ bucket4j-springboot/src/main/java/com/study/bucket4jspringboot/config/RateLimiterConfig.java
@@ -1,12 +1,15 @@
 package com.study.bucket4jspringboot.config;
 
 import io.github.bucket4j.Bandwidth;
-import io.github.bucket4j.Bucket;
 import io.github.bucket4j.BucketConfiguration;
 import io.github.bucket4j.Refill;
 import io.github.bucket4j.distributed.ExpirationAfterWriteStrategy;
 import io.github.bucket4j.redis.lettuce.cas.LettuceBasedProxyManager;
 import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.codec.ByteArrayCodec;
+import io.lettuce.core.codec.RedisCodec;
+import io.lettuce.core.codec.StringCodec;
 import java.time.Duration;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,7 +22,7 @@ import org.springframework.context.annotation.Configuration;
 public class RateLimiterConfig {
 
     // 버킷에 담길 수 있는 토큰의 최대 수
-    private static final int CAPACITY = 3;
+    private static final int CAPACITY = 20;
 
     // REFILL_DURATION 으로 지정된 시간 동안 버킷에 추가될 토큰의 수
     private static final int REFILL_TOKEN_AMOUNT = 3;
@@ -31,7 +34,9 @@ public class RateLimiterConfig {
 
     @Bean
     public LettuceBasedProxyManager lettuceBasedProxyManager() {
-        return LettuceBasedProxyManager.builderFor(lettuceRedisClient)
+        StatefulRedisConnection<String, byte[]> connect = lettuceRedisClient.connect(RedisCodec.of(StringCodec.UTF8, ByteArrayCodec.INSTANCE));
+
+        return LettuceBasedProxyManager.builderFor(connect)
             // 버킷 만료 정책(최대 용량으로 채워지는데 필요한 60초 동안 버킷으로 유지)
             .withExpirationStrategy(ExpirationAfterWriteStrategy.basedOnTimeForRefillingBucketUpToMax(Duration.ofSeconds(60)))
             .build();
@@ -40,12 +45,7 @@ public class RateLimiterConfig {
     @Bean
     public BucketConfiguration bucketConfiguration() {
         return BucketConfiguration.builder()
-            .addLimit(Bandwidth.classic(CAPACITY, Refill.intervally(REFILL_TOKEN_AMOUNT, REFILL_DURATION)))
-            .build();
-    }
-
-    public Bucket initBucket(String apiKey) {
-        return Bucket.builder()
+            // REFILL_DURATION 동안 REFILL_TOKEN_AMOUNT 만큼의 토근이 버킷에 추가(5초동안 3개의 토큰이 추가됨)
             .addLimit(Bandwidth.classic(CAPACITY, Refill.intervally(REFILL_TOKEN_AMOUNT, REFILL_DURATION)))
             .build();
     }
