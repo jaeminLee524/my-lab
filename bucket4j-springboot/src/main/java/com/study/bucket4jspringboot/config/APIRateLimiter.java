4b8f002fd3d77856371f8852c5691855496f9cfa
이재민 <rkwoals524@hanmail.net>
2024-05-16 10:13:33 +0900

    bucket4j with redis config 설정

---
+++ bucket4j-springboot/src/main/java/com/study/bucket4jspringboot/config/APIRateLimiter.java
@@ -0,0 +1,44 @@
+package com.study.bucket4jspringboot.config;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.BucketConfiguration;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class APIRateLimiter {
+
+    // 버킷에 담길 수 있는 토큰의 최대 수
+    private static final int CAPACITY = 3;
+
+    // REFILL_DURATION 으로 지정된 시간 동안 버킷에 추가될 토큰의 수
+    private static final int REFILL_TOKEN_AMOUNT = 3;
+
+    // 토큰이 재충전되는 빈도
+    private static final Duration REFILL_DURATION = Duration.ofSeconds(5);
+
+    private final BucketManager bucketManager;
+
+    public APIRateLimiter(BucketManager bucketManager) {
+        this.bucketManager = bucketManager;
+    }
+
+    public boolean tryConsume(String apiKey) {
+        BucketConfiguration bucketConfiguration = createBucketConfiguration();
+        boolean tryConsume = bucketManager.getOrCreateBucket(apiKey, bucketConfiguration).tryConsume(1);
+
+        log.info("API Key: {}, tryConsume: {}, tryTime: {}", apiKey, tryConsume, LocalDateTime.now());
+
+        return tryConsume;
+    }
+
+    private BucketConfiguration createBucketConfiguration() {
+        return BucketConfiguration.builder()
+            .addLimit(Bandwidth.simple(CAPACITY, REFILL_DURATION).withInitialTokens(REFILL_TOKEN_AMOUNT))
+            .build();
+    }
+}
---
+++ bucket4j-springboot/src/main/java/com/study/bucket4jspringboot/config/LettuceRedisConfig.java
@@ -0,0 +1,40 @@
+package com.study.bucket4jspringboot.config;
+
+import io.lettuce.core.RedisClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisKeyValueAdapter.EnableKeyspaceEvents;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableRedisRepositories(enableKeyspaceEvents = EnableKeyspaceEvents.ON_STARTUP)
+public class LettuceRedisConfig {
+
+    private static final String PREFIX = "redis://";
+    private static final String HOST = "127.0.0.1";
+    private static final int POST = 6379;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(HOST, POST);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericToStringSerializer<>(Object.class));
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisClient redisClient() {
+        return RedisClient.create(PREFIX + HOST + ":" + POST);
+    }
+}
