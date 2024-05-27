009a32416615c72adf034cc1701741b115d7d01c
이재민 <rkwoals524@hanmail.net>
2024-05-27 14:23:21 +0900

    initial commit

---
+++ spring-sse-with-redis/src/main/java/com/study/springssewithredis/SpringSseWithRedisApplication.java
@@ -0,0 +1,13 @@
+package com.study.springssewithredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSseWithRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSseWithRedisApplication.class, args);
+	}
+
+}
---
+++ spring-sse-with-redis/src/test/java/com/study/springssewithredis/SpringSseWithRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springssewithredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSseWithRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
